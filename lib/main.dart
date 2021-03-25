163a6874d263e85c70633127bd82d0081e2322d8
fercansen <fercansen@gmail.com>
2021-03-25 16:27:21 +0300

    Text listview is genrerated with the button

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flowing Text",
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -29,12 +29,36 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("AppNation"),
-      ),
-      body: Center(
-        child: Text("AppNation"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("AppNation Flowing Text"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 5),
+                RaisedButton(
+                  child: Text("Auto Scroll"),
+                  // todo: add current status of the scrolling here
+                  onPressed: () {},
+                ),
+                ListView.builder(
+                  physics: ScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        "This is a sample text to be scrolled",
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
